83f9ba692a6cf5377ba72b057a63cc341c237b4a
Stephane0713 <stephaned2205@gmail.com>
2023-11-24 12:43:02 +0100

    #7 done

--- src/tp11/state/CommandeExpediee.java
+++ src/tp11/state/CommandeExpediee.java
@@ -1,6 +1,7 @@
 package tp11.state;
 
 
+import tp11.Commande;
 
 /**
  * Etat représentant la commande expédiée.
@@ -8,5 +9,10 @@ package tp11.state;
  * En revanche on va modifier le status de la commande par : Livrée
  */
 public class CommandeExpediee implements CommandeState {
-    // TODO à vous d'écrire ce qu'il faut
+
+    @Override
+    public void next(Commande commande) {
+        commande.setStatus("Livrée");
+        commande.setCurrentState(null);
+    }
 }
